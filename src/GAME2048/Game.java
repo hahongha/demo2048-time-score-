7dd5448ac0d407abb76140e0c37c88984dea5cbf
hahongha <phamha03122003@gmail.com>
2023-04-09 16:37:42 +0700

    demomusic

--- src/GAME2048/Game.java
+++ src/GAME2048/Game.java
@@ -11,27 +11,23 @@ import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
 
 public class Game extends JPanel implements KeyListener, Runnable {
-	private static final long SerialVersionUID = 1L;
-	public static final int WIDTH = 500;
-	public static final int HEIGHT =700;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L; // thoi gian chay tuan tu hoa lien ket
+	public static final int WIDTH = Gameboard.BOARD_WIDTH+ 2* Gameboard.SPACING;
+	public static final int HEIGHT = Gameboard.BOARD_WIDTH + 250;
 	public static final Font main = new Font("Bebas Neue Regular", Font.PLAIN, 28);
 	private Thread game;
 	private boolean running;
 	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 	private Gameboard board;
-	
-	
-	private long startTime;
-	private long elapsed;
-	private boolean set;
 
-	//khoi tao bang
 	public Game() {
 		setFocusable(true);
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		addKeyListener(this);
-		
-		board = new Gameboard(WIDTH/2-Gameboard.BOARD_WIDTH/2, HEIGHT-Gameboard.BOARD_HEIGHT-10);
+		board = new Gameboard(WIDTH / 2 - Gameboard.BOARD_WIDTH / 2, HEIGHT - Gameboard.BOARD_HEIGHT - 10);
 	}
 
 	private void update() {
@@ -42,10 +38,9 @@ public class Game extends JPanel implements KeyListener, Runnable {
 
 	private void render() {
 		Graphics2D g = (Graphics2D) image.getGraphics();
-		g.setColor(Color.WHITE);
+		g.setColor(Color.white);
 		g.fillRect(0, 0, WIDTH, HEIGHT);
 		board.render(g);
-		// render board
 		g.dispose();
 
 		Graphics2D g2d = (Graphics2D) getGraphics();
@@ -53,12 +48,11 @@ public class Game extends JPanel implements KeyListener, Runnable {
 		g2d.dispose();
 	}
 
-	//fps : toc do khung tinh moi giay
 	@Override
 	public void run() {
-		int fps = 0, update = 0;
-		long fpsTimer = System.currentTimeMillis();
-		double nsPerUpdate = 1000000000.0 / 60;
+		int fps = 0, update = 0;//fps và số lần update
+		long fpsTimer = System.currentTimeMillis();//bộ đếm thời gian fps tính bằng miliseconds
+		double nsPerUpdate = 1000000000.0 / 60;//có bao nhiêu nanosenconds giữa các lần cập nhật với nhau
 
 		// last update time in nanosecond
 		double then = System.nanoTime();
@@ -92,7 +86,7 @@ public class Game extends JPanel implements KeyListener, Runnable {
 			}
 		}
 
-		// FPS Timer
+//		 FPS Timer
 		if (System.currentTimeMillis() - fpsTimer > 1000) {
 			System.out.printf("%d fps %d updates", fps, update);
 			System.out.println();
@@ -127,11 +121,18 @@ public class Game extends JPanel implements KeyListener, Runnable {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		Keyboard.keyPressed(e);
+		if(e.getKeyCode()== KeyEvent.VK_X) {
+			stop();
+		}
+		if (e.getKeyCode()==KeyEvent.VK_MULTIPLY) {
+			board.play.volumeMute();
+//			if (!playhasStarted)
+//				hasStarted = true;
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		Keyboard.keyReleased(e);
 	}
-
 }
--- src/GAME2048/Tile.java
+++ src/GAME2048/Tile.java
@@ -8,10 +8,10 @@ import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 
 public class Tile {
-	public static final int WIDTH = 100;
-	public static final int HEIGHT = 100;
-	public static final int SLIDE_SPEED =30;
-	public static final int ARC_WIDTH = 15;
+	public static final int WIDTH = 100;//chiều dài
+	public static final int HEIGHT = 100;//chiều rộng
+	public static final int SLIDE_SPEED =10;
+	public static final int ARC_WIDTH = 15;//đô bo góc
 	public static final int ARC_HEIGHT = 15;
 
 	private int value;
@@ -26,24 +26,25 @@ public class Tile {
 	private boolean beginningAnimation= true;
 	private double scaleFirst = 0.1;//kich thuoc o thay doi khi 2 o key hop
 	private BufferedImage beginningImage;
-	private boolean canAnimation= false;
+//	private boolean canAnimation= false;
 	
 	private boolean combineAnimation=false;
 	private double scaleCombine= 1.3;
 	private BufferedImage combineImage;
 	private boolean canCombine =true;
 
+	/*hàm khởi tạo bởi giá trị ô và các vị trí ô*/
 	public Tile(int value, int x, int y) {
 		this.value = value;
 		this.x = x;
 		this.y = y;
 		slideTo = new Point(x, y);
-		tileImage = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+		tileImage = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);//hình ảnh đệm cho ô
 		beginningImage= new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
 		combineImage= new BufferedImage(WIDTH*2, HEIGHT*2, BufferedImage.TYPE_INT_ARGB);
 		drawImage();
 	}
-
+	// hàm vẽ ô
 	private void drawImage() {
 		Graphics2D g = (Graphics2D) tileImage.getGraphics();
 		if (value == 2) {
@@ -83,15 +84,6 @@ public class Tile {
 			background = Color.black;
 			text = Color.white;
 		}
-//			else if(value == 0){
-//				background = Color.lightGray;
-//				text = Color.WHITE;
-//			}
-//			else{
-//				background = new Color(0x000000);
-//				text = new Color(0xffffff);
-//			}
-
 		g.setColor(new Color(0, 0, 0, 0));
 		g.fillRect(0, 0, WIDTH, HEIGHT);
 
@@ -106,13 +98,15 @@ public class Tile {
 			font = Game.main.deriveFont(40f);
 		}
 		g.setFont(font);
-		//set vi tri cua text so voi o
+		//set vi tri cua text so voi o do java viết từ trái -  dưới
 		int drawX = WIDTH/2 -DrawUtils.getMessageWidth(""+value, font, g)/2;
-		int drawY = HEIGHT - DrawUtils.getMessageHeight(""+value, font, g);
+		//từ tâm của ô thì ta dịch chuyển sang bên trái 1 nửa giá trị
+		int drawY = HEIGHT/2 + DrawUtils.getMessageHeight(""+value, font, g)/2;
+		//từ tâm của ô thì ta dịch chuyển xuống dưới 1 nửa giá trị
 		g.drawString(""+value, drawX, drawY);
 		g.dispose();
 	}
-	
+	//animation
 	public void update() {
 		if(beginningAnimation) {
 			AffineTransform transform = new AffineTransform();
@@ -145,7 +139,7 @@ public class Tile {
 		}
 		
 	}
-	
+	//hiển thị
 	public void render(Graphics2D g) {
 		if(beginningAnimation) {
 			g.drawImage(beginningImage, x, y, null);
@@ -153,7 +147,7 @@ public class Tile {
 			g.drawImage(combineImage, (int)(x+WIDTH/2-scaleCombine*WIDTH/2), 
 					(int)(y+HEIGHT/2-scaleCombine*HEIGHT/2), null);
 		}else {
-			g.drawImage(tileImage, x, y, null);
+			g.drawImage(tileImage, x, y, null);//hiển thị ô tại vị trí chỉ định
 		}
 		
 	}
